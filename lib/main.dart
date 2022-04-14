2cebfc5f907e91eb8278aee90c5e1ae25ad534d1
Maicon J Duarte <duartejosemaicon@gmail.com>
2022-04-14 00:32:17 -0300

    Buscando os valores via api. Iniciando layout da tela.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
-void main() {
-  runApp();
+const requestUrl = "https://api.hgbrasil.com/finance?key=00d542b4";
+
+void main() async {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
 }
+
+Future<Map> getData() async {
+  http.Response response = await http.get(Uri.parse(requestUrl));
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                child: Text(
+                  "Carregando Dados...",
+                  style: TextStyle(color: Colors.amber, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            default:
+              if(snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    "Erro ao Carrgar os Dados ",
+                    style: TextStyle(color: Colors.amber, fontSize: 25),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else {
+                return Container(color: Colors.green,);
+              }
+          }
+        },
+      ),
+    );
+  }
+}
+
+
+/*
+  theme: ThemeData(
+      hintColor: Colors.amber,
+      primaryColor: Colors.white,
+      inputDecorationTheme: InputDecorationTheme(
+        enabledBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        focusedBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+        hintStyle: TextStyle(color: Colors.amber),
+      )),
+));
+ */
