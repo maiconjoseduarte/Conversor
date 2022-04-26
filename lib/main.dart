c45d961e2087f7a12a05ed32119287a02a1eaeee
Maicon J Duarte <duartejosemaicon@gmail.com>
2022-04-26 00:20:00 -0300

    Inserido os campos e aplicado regra para converter os valores informados.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
@@ -8,6 +9,17 @@ const requestUrl = "https://api.hgbrasil.com/finance?key=00d542b4";
 void main() async {
   runApp(MaterialApp(
     home: Home(),
+    theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+          hintStyle: TextStyle(color: Colors.amber),
+        )
+    ),
   ));
 }
 
@@ -24,6 +36,53 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  late double dolar;
+  late double euro;
+
+  void _realChange(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+
+    double real = double.parse(text);
+    dolarController.text = (real/dolar).toStringAsFixed(2);
+    euroController.text = (real/euro).toStringAsFixed(2);
+  }
+
+  void _dolarChange(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+
+    double dolar = double.parse(text);
+    realController.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
+  }
+
+  void _euroChange(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+  }
+
+  void _clearAll() {
+    realController.text = '';
+    dolarController.text = '';
+    euroController.text = '';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,7 +106,7 @@ class _HomeState extends State<Home> {
                 ),
               );
             default:
-              if(snapshot.hasError) {
+              if (snapshot.hasError) {
                 return Center(
                   child: Text(
                     "Erro ao Carrgar os Dados ",
@@ -56,7 +115,23 @@ class _HomeState extends State<Home> {
                   ),
                 );
               } else {
-                return Container(color: Colors.green,);
+                dolar = snapshot.data!["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data!["results"]["currencies"]["EUR"]["buy"];
+
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Icon(Icons.monetization_on, size: 130, color: Colors.amber),
+                      builTextField("Reais", "R\$", realController, _realChange),
+                      Divider(),
+                      builTextField("Dolares", "US\$", dolarController, _dolarChange),
+                      Divider(),
+                      builTextField("Euros", "€", euroController, _euroChange)
+                    ],
+                  ),
+                );
               }
           }
         },
@@ -65,17 +140,18 @@ class _HomeState extends State<Home> {
   }
 }
 
-
-/*
-  theme: ThemeData(
-      hintColor: Colors.amber,
-      primaryColor: Colors.white,
-      inputDecorationTheme: InputDecorationTheme(
-        enabledBorder:
-            OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-        focusedBorder:
-            OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
-        hintStyle: TextStyle(color: Colors.amber),
-      )),
-));
- */
+Widget builTextField(String label, String prefix, TextEditingController controller, fun) {
+ return TextField(
+   controller: controller,
+   decoration: InputDecoration(
+     labelText: label,
+     labelStyle: TextStyle(color: Colors.amber),
+     border: OutlineInputBorder(),
+     prefixText: prefix + " ",
+   ),
+   style: TextStyle(
+       color: Colors.amber, fontSize: 25
+   ),
+   onChanged: fun,
+ );
+}
